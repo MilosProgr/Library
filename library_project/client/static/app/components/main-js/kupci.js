7f1e75154e37b4a383641081cacfa1246e7cf4e7
Milos Petkovic <petkovicam@gmail.com>
2025-11-13 22:41:50 +0100

    update
    quick update

--- library_project/client/static/app/components/main-js/kupci.js
+++ library_project/client/static/app/components/main-js/kupci.js
@@ -1,5 +1,6 @@
+
 export default {
-    template:`
+    template: `
 <div class="w-75 p-3" v-if="stranicaZaPrikaz=='kupci'">
     <kupac-form v-on:sacuvaj="createKupac" v-bind:greska="greska" v-bind:naslov="'Dodaj kupca'" v-bind:dugme="'Dodaj'"></kupac-form>
 </div>
@@ -9,28 +10,28 @@ export default {
     <tabela-kupca v-bind:naslov="'Tabela kupaca'" v-bind:kupci="kupci" v-on:uklanjanje="removeKupac" v-on:izmena="setKupacZaIzmenu"></tabela-kupca>
 </div>
     `,
-    data(){
+    data() {
         return {
-            kupci:[],
-            
-            kupacZaIzmenu:{},
-        
-            stranicaZaPrikaz:"",
+            kupci: [],
+
+            kupacZaIzmenu: {},
+
+            stranicaZaPrikaz: "",
 
             greska: "nemaGreske",
-            
+
             validations: {}
         }
     },
-    methods:{
-        setKupacZaIzmenu(kupac){ //skladistenje nakon izmene//
+    methods: {
+        setKupacZaIzmenu(kupac) { //skladistenje nakon izmene//
             this.$router.push(`/kupci/${kupac.IDKupac}`);
         },
 
-        refreshKupac(){
+        refreshKupac() {
             axios.get("api/kupci").then((response) => {
                 ////Stil datuma u tabeli
-                for(let d of response.data) {
+                for (let d of response.data) {
                     d.datumRodjenja = new Date(d.datumRodjenja).toDateString().split("Z")[0];
                 }
                 this.kupci = response.data;
@@ -38,48 +39,48 @@ export default {
         },
 
         //dodavanje//
-        createKupac(kupac){
+        createKupac(kupac) {
             this.greska = "nemaGreske";
-            if(new Date(kupac.datumRodjenja) > new Date()){
+            if (new Date(kupac.datumRodjenja) > new Date()) {
                 this.greska = "greska";
             }
-            else{
+            else {
                 axios.post("api/kupci", kupac).then((response) => {
                     this.refreshKupac();
                 });
-    
-                
+
+
             }
         },
 
         //izmena//
-        updateKupac(kupac){
+        updateKupac(kupac) {
             this.greska = "nemaGreske";
-            if(new Date(kupac.datumRodjenja) < new Date() && new Date() < age){
+            if (new Date(kupac.datumRodjenja) < new Date() && new Date() < age) {
                 this.greska = "greska";
             }
-            else{
+            else {
                 axios.put(`api/kupci/${kupac.IDKupac}`, kupac).then((response) => {
-                this.refreshKupac();
-            });
+                    this.refreshKupac();
+                });
             }
-            
+
         },
 
         //brisanje//
-        removeKupac(IDKupac){
+        removeKupac(IDKupac) {
             axios.delete(`api/kupci/${IDKupac}`).then((response) => {
                 this.refreshKupac();
             });
         },
 
-        navigate(page){
+        navigate(page) {
             this.stranicaZaPrikaz = page;
         },
-        
+
 
     },
-    created(){
+    created() {
         this.refreshKupac();
     }
 }
